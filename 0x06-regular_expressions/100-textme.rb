2e1f16d03e047e8177489a749cc0953a71a525b8
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-08-01 20:53:54 +0000

    Get the senders and the flags from the logfile

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/\[from:(.*?)\]\[to:(.*?)\]\[flags:(.*?)\]/).join(',')
+# puts ARGV[0].scan(/\[from:(.*?)\]\[to:(.*?)\]\[flags:(.*?)\]/).join(',')
+
+input_string = ARGV[0]
+
+from_value = input_string.scan(/\[from:([^]]*)\]/).flatten.first
+to_value = input_string.scan(/\[to:([^]]*)\]/).flatten.first
+flags_value = input_string.scan(/\[flags:([^]]*)\]/).flatten.first
+
+result = [from_value, to_value, flags_value].join(',')
+puts result

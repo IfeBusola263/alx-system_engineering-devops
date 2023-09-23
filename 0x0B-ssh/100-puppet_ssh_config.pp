89ef7ef6a2e96ab39eee6b09b3c8bc4e1c6d174b
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-09-23 12:28:16 +0000

    Configuration management manifest for my local ssh client

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # This manifest configures the ssh configuration file
 
-file { '/etc/ssh/ssh_config':
+file { '/etc/ssh/*conf':
   ensure  =>  present,
-  content => '\nPasswordAuthentication no\nIdentifyFile ~/.ssh/school',
+  content => '\nPasswordAuthentication no\nIdentityFile ~/.ssh/school',
   }

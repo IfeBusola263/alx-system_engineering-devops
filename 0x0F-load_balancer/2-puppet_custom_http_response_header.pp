50bdd7af8375eb2911e8c4367984b2d4f8ea8c75
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-03 05:34:31 +0000

    install nginx with custom header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,3 +12,21 @@ file {'/var/www/html/index.html':
 file { '/etc/nginx/sites-enabled/':
   ensure  =>  absent,
 }
+
+file { '/etc/nginx/sites-available/default':
+  ensure  =>  present,
+}
+-> file_line { 'including custom header':
+  path  =>  '/etc/nginx/sites-available/default',
+  line  =>  "# as directory, then fall back to displaying a 404.\n\t\tadd_header X-Served-By \"${HOSTNAME}\";",
+  match => '^# as directory, then fall back to displaying a 404.$',
+}
+
+file {'/etc/nginx/sites-enabled/default':
+  ensure =>  present,
+  link   =>  '/etc/nginx/sites-available/default',
+}
+
+service { 'nginx':
+  ensure  =>  running,
+}

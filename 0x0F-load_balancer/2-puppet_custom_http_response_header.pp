c0a81bd8e1ff24e30e34ecf8ede9efb7634427d3
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-03 18:17:27 +0000

    quick check

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,28 +3,29 @@ include stdlib
 
 package {'nginx':
   ensure  =>  installed,
-}
+  }
 
 file {'/var/www/html/index.html':
   ensure  =>  present,
   content =>  'Hello World!',
-}
+  }
 
 file { '/etc/nginx/sites-enabled/def*':
   ensure  =>  absent,
-}
+  }
 
 file_line { 'including custom header':
   path  =>  '/etc/nginx/sites-available/default',
-  line  =>  "\t\t# as directory, then fall back to displaying a 404.\n\t\tadd_header X-Served-By \"${fact['networking']['hostname']}\";",
+  line  =>  "\t\t# as directory, then fall back to displaying a 404.\
+\n\t\tadd_header X-Served-By \"${::hostname}\";",
   match =>  '# as directory, then fall back to displaying a 404.',
-}
+  }
 
 file {'/etc/nginx/sites-enabled/default':
-  ensure =>  present,
-  link   =>  '/etc/nginx/sites-available/default',
-}
+  ensure   =>  link,
+  target   =>  '/etc/nginx/sites-available/default',
+  }
 
 service { 'nginx':
   ensure  =>  running,
-}
+  }

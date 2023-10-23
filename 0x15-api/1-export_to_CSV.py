a5be94774fb85cc7431ea3a4a91438a6f58b8fb7
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-23 17:14:15 +0000

    included the quote_all argument for quoting in the csv writer

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,24 +15,20 @@ if __name__ == '__main__':
     to_do = requests.get(
         'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
 
-    # fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
     rows = []
     name = user.json().get('username')
 
     for task in to_do.json():
         row = []
-        row.append(argv[1])
-        row.append(name)
-        row.append(task["completed"])
-        row.append(task["title"])
+        row.append(str(argv[1]))
+        row.append(str(name))
+        row.append(str(task["completed"]))
+        row.append(str(task["title"]))
         rows.append(row)
 
     with open(f'{argv[1]}.csv', 'w', encoding='utf-8') as csv_file:
         # create a csv writer object
-        writer = csv.writer(csv_file)
-
-        # writing the column title or fields
-        # writer.writerow(fields)
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
 
         # write the data
         writer.writerows(rows)

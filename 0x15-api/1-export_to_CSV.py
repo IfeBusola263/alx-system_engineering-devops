6df9ad6baa37eac11d49c7c72a818803c8809ef5
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-10-23 16:36:52 +0000

    compiled in csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This module requests the todo list of an employee, and diplay it in
+CSV  format.
+EMPLOYEE_NAME and TASK_TITLE
+"""
+
+if __name__ == '__main__':
+    import csv
+    import requests
+    from sys import argv
+
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+
+    to_do = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+
+    fields = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+    rows = []
+    name = user.json().get('username')
+
+    for task in to_do.json():
+        row = []
+        row.append(argv[1])
+        row.append(name)
+        row.append(task["completed"])
+        row.append(task["title"])
+        rows.append(row)
+
+    with open('USER_ID.csv', 'w', encoding='utf-8') as csv_file:
+        # create a csv writer object
+        writer = csv.writer(csv_file)
+
+        # writing the column title or fields
+        writer.writerow(fields)
+
+        # write the data
+        writer.writerows(rows)

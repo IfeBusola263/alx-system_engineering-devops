5122b575000bb0a0086441504e82973eac59f4a1
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-11-10 16:41:18 +0000

    Corrected path syntax in the manifest

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # This puppet manifest fixes an internal server error issue in a server running apache2
 
 exec { 'replace_string_in_php_settings':
-  command =>  'sed -i s|phpp|php|g /var/www/html/wp-settings.php',
+  command =>  'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
   path    =>  '/usr/local/bin:bin',
 }

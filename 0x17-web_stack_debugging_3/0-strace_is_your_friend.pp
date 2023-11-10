3cbc96a38a4ab79eff1d0ce83dbd274830e703b8
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-11-10 14:43:31 +0000

    Puppet to correct the settings file for extension error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # This puppet manifest fixes an internal server error issue in a server
 # running apache2
 
-exec { 'sed -i "s|phpp|php|" var/www/html/wp-settings.php':
-  path  => '/usr/bin',
+exec { 'replace_string_in_php_settings':
+  command =>  'sed -i "s|phpp|php|" /var/www/html/wp-settings.php',
+  path    =>  '/usr/bin',
 }

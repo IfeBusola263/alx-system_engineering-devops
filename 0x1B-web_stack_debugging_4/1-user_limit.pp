8481cbab5a2fd8ed068d4091648bd42ffe825157
Busola Ogunbayo <ifebusolaj95@gmail.com>
2023-11-16 19:30:17 +0000

    Puppet manifest to change the file open limit of a user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,12 @@
 # This manifest reconfigures the limit of a user to increase the number of files
 # that can be opened by the user
 
-file_line { 'Hard limit configuration':
-  path  => '/etc/security/limits.conf',
-  line  => 'holberton hard nofile 500',
-  match => '^holberton hard nofile 5$',
+exec { 'Hard limit configuration':
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 500/g" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
 
-file_line { 'Soft limit configuration':
-  path  => '/etc/security/limits.conf',
-  line  => 'holberton soft nofile 400',
-  match => '^holberton soft nofile 4$',
+exec { 'Soft limit configuration':
+  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 400/g" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
